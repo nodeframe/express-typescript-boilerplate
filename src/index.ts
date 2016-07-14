9056cec47216650dcdbf987075e84f2e8b741df5
thakerng <southbridge_855@hotmail.com>
2016-07-14 18:06:37 +0700

    index.js

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import * as express from 'express';
-import * as seneca from 'seneca';
 const app = express();
 
 app.get('/',(req,res)=>{
   res.send('Hello World');
-});
-
-app.get('/api/payment',(req,res)=>{
-  res.send({success:true});
 });
 
 export default app;
